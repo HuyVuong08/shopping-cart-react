42f219a80dacc77ee4f936b0972a5a3b9294cb58
HuyVuong08 <huy.vuongle123@gmail.com>
2023-06-22 20:45:35 +0800

    Update to remove warnings

--- src/index.js
+++ src/index.js
@@ -143,7 +143,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
         w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
         var f = d.getElementsByTagName(s)[0],
                 j = d.createElement(s),
-                dl = l != "dataLayer" ? "&l=" + l : "";
+                dl = l !== "dataLayer" ? "&l=" + l : "";
         j.async = true;
         j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
         f.parentNode.insertBefore(j, f);
