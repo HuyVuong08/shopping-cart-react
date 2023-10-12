65fa73d2d2095a16d2b0432a5e7688451c063d76
HuyVuong08 <49582049+HuyVuong08@users.noreply.github.com>
2023-10-12 09:09:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -211,6 +211,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
                                         value: 10.0, 
                                         currency: 'USD',
                                 });
+                                // ttq.track('AddToCart', {
+                                //         content_name: 'book', 
+                                //         value: 10,0, 
+                                //         currency: 'USD',
+                                // });
                                 // ttq.track('ClickButton', {
                                 //     contents: [
                                 //     {
