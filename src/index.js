7811f28583158180700fdae99d1f4a06a25f67d8
HuyVuong08 <49582049+HuyVuong08@users.noreply.github.com>
2023-10-12 10:23:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -211,28 +211,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
                                         value: 10.0, 
                                         currency: 'USD',
                                 });
-                                ttq.track('AddToCart', {
-                                        content_name: 'book',
-                                        content_ids: '12345678',
-                                        value: 10.0,
-                                        currency: 'USD',
-                                });
-                                ttq.identify({
-                                        sha256_email: '0c7e6a405862e402eb76a70f8a26fc732d07c32931e9fae9ab1582911d2e8a3b',
-                                        sha256_phone_number: '9f7ec22d72092cd3c0b58726ed9c2d91b92e51a3f29837508fb2948bb22dd2fd',
-                                })
-                                ttq.track('AddToCart',{
-                                        content_id: '301',
-                                        content_type: 'product',
-                                        content_name:'dress',
-                                        quantity: 1,
-                                });
-                                ttq.track('SecondPurchse',{
-                                        content_name: 'book', 
-                                        value: 10.0, 
-                                        currency:'USD',
-                                });
-
                                 // ttq.track('AddToCart', {
                                 //         content_name: 'book', 
                                 //         value: 10,0, 
