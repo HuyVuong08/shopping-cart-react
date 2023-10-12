cfff30047fad8386613d2577dad32deb345932ca
HuyVuong08 <49582049+HuyVuong08@users.noreply.github.com>
2023-10-12 09:17:27 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -220,7 +220,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
                                 ttq.identify({
 sha256_email: '0c7e6a405862e402eb76a70f8a26fc732d07c32931e9fae9ab1582911d2e8a3b',
 sha256_phone_number: '9f7ec22d72092cd3c0b58726ed9c2d91b92e51a3f29837508fb2948bb22dd2fd',
-}}
+})
 ttq.track('AddToCart',{
 Vuong Le Huy
 content_id: '301',
