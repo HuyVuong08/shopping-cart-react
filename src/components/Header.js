027f6d9a2546d56ab31615f7dce0d7c6719bc28d
HuyVuong08 <huy.vuongle123@gmail.com>
2024-10-22 14:20:33 -0400

    Update precision of the price to 2 decimal places

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,7 +61,7 @@ const Header = () => {
                       />
                       <div className="cartItemDetail">
                         <span>{prod.name}</span>
-                        <span>$ {prod.price.split(".")[0]}</span>
+                        <span>$ {parseFloat(prod.price).toFixed(2)}</span>
                       </div>
                       <AiFillDelete
                         fontSize="20px"
