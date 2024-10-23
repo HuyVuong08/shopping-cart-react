c9e64d088bf80dcad6ee5bc4ed408f7ef418f600
HuyVuong08 <huy.vuongle123@gmail.com>
2024-10-23 15:25:13 -0400

    Update Image size

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 import { createContext, useContext, useReducer } from "react";
 import { cartReducer, productReducer } from "./Reducers";
 
@@ -11,13 +11,18 @@ const Context = ({ children }) => {
     id: faker.string.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.image.urlPicsumPhotos({ width: 350, height: 300, blur: 0, grayscale: false  }),
+    image: faker.image.urlPicsumPhotos({
+      width: 200,
+      height: 150,
+      blur: 0,
+      grayscale: false,
+    }),
     inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
     ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
   }));
 
-  console.log(products)
+  console.log(products);
 
   const [state, dispatch] = useReducer(cartReducer, {
     products: products,
